9c227a9fadece88c561c261fb0bae1f8add5e7f8
JFrosti <frosti.2@osu.edu>
2018-12-11 19:47:02 -0500

    Adding in two default scripts
    Adding an Alert and Confirm script by default, to show users an example and to allow for quick testing of the extension itself on install.

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({
-    scriptNames: ""
-  }, function () {
-  });
+  chrome.storage.sync.set({scriptNames: "Alert;Confirm"}, function () {});
+  chrome.storage.sync.set({Alert: "alert('Alert Test');"}, function () {});
+  chrome.storage.sync.set({Confirm: "alert(confirm('Confirm Test'));"}, function () {});
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
